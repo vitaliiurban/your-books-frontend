6f14f469adce88d51d0f943ab1747d46ff43d7b8
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2023-12-02 18:36:50 +0200

    for uraban

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>hey it's home</div>;
+  return (
+    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 flex justify-center flex-col items-center">
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        it's a home page{" "}
+        <span className="text-orange-600">You can search for books here </span>
+      </h2>
+    </div>
+  );
 }
 
 export default Home;
